corrected bugs in loss computation

helpers.py
@@ -5,16 +5,24 @@ def softmax(x):
     exp = np.exp(x)
     return exp / exp.sum(axis=1, keepdims=True)
 
-def cross_entropy_loss(q, p):
+def cross_entropy_loss(logits, p):
+    logits -= np.max(logits, axis=1, keepdims=True)
+
     correct_class = np.argmax(p, axis=1)
-    row = np.arange(q.shape[0])
-    return np.log(np.exp(q).sum(axis=1)) - q[row, correct_class]
+    row = np.arange(logits.shape[0])
+    return np.log(np.exp(logits).sum(axis=1)) - logits[row, correct_class]
 
 def cross_entropy_gradient(q, p):
     return q - p
 
-def accuracy(logits, true_class):
-    probs = np.argmax(softmax(logits), axis=1)
-    true = np.argmax(true_class, axis=1)
+def loss_and_accuracy(model, X, true_label):
+
+    logits = model.forward(X)
+
+    loss = cross_entropy_loss(logits, true_label).mean()
+
+    predicted = np.argmax(softmax(logits), axis=1)
+    true = np.argmax(true_label, axis=1)
+    accuracy = np.mean(predicted == true)
     
-    return np.mean(probs == true)
+    return loss, accuracy

mnist_data.py
@@ -1,29 +1,18 @@
-from sklearn.datasets import fetch_openml
-from sklearn.preprocessing import StandardScaler, MinMaxScaler, OneHotEncoder
-from sklearn.pipeline import make_pipeline
-from numpy import atleast_2d
+import numpy as np
 
+from sklearn.datasets import fetch_openml # pyright: ignore[reportMissingModuleSource]
+from sklearn.preprocessing import OneHotEncoder, StandardScaler, MinMaxScaler # pyright: ignore[reportMissingModuleSource]
+from sklearn.pipeline import make_pipeline # pyright: ignore[reportMissingModuleSource]
+
+### Prepare data
 mnist = fetch_openml('mnist_784', as_frame=False)
 
+scaler = make_pipeline(MinMaxScaler(feature_range=(-.25, .25)), StandardScaler(with_std=False))
+onehot = OneHotEncoder(categories=[np.arange(10)], sparse_output=False)
 
 X = mnist['data']
-y = atleast_2d(mnist['target'].astype(int)).T
+y = onehot.fit_transform(mnist['target'].astype(int)[:, np.newaxis])
 
-scaler = make_pipeline(MinMaxScaler((-.1, .1)), StandardScaler(with_std=False))
-onehot = OneHotEncoder(categories=[range(10)], sparse_output=False)
-
-N_train = 50_000
-N_valid = 10_000
-N_test  = 10_000
-
-X_train = X[:N_train]
-X_valid = X[N_train:N_train+N_valid]
-X_test  = X[-N_test:]
-
-X_train = scaler.fit_transform(X_train.reshape((-1, 1))).reshape((N_train, -1))
-X_valid = scaler.transform(X_valid.reshape((-1, 1))).reshape((N_valid, -1))
-X_test  = scaler.transform(X_test.reshape((-1, 1))).reshape((N_test, -1))
-
-y_train = onehot.fit_transform(y[:N_train])
-y_valid = onehot.fit_transform(y[N_train:N_train+N_valid])
-y_test  = onehot.fit_transform(y[-N_test:])
+X_train, y_train = scaler.fit_transform(X[:50_000])  , y[:50_000]
+X_valid, y_valid = scaler.transform(X[50_000:60_000]), y[50_000:60_000]
+X_test,  y_test  = scaler.transform(X[60_000:])      , y[60_000:]

train.py
@@ -1,8 +1,9 @@
 import numpy as np
+import copy
 
 from two_layer_nn import TwoLayerMLP
 from mnist_data import X_train, y_train, X_valid, y_valid, X_test, y_test
-from helpers import cross_entropy_loss, cross_entropy_gradient, accuracy
+from helpers import softmax, cross_entropy_gradient, loss_and_accuracy
 
 
 hyperparam = {
@@ -12,7 +13,7 @@ hyperparam = {
     'output_size': y_train.shape[1],
     'learning_rate': 1e-1,
     'lr_decay': 0.99,
-    'n_epochs': 20,
+    'n_epochs': 10,
     'batch_size': 32,
     'random_seed': 0,
 }
@@ -35,6 +36,7 @@ y_test  = y_test.astype( hyperparam['dtype'])
 
 
 n_batches = np.ceil(X_train.shape[0] // hyperparam['batch_size']).astype(int)
+best_loss = np.inf
 
 for epoch in range(1, 1 + hyperparam['n_epochs']):
     for batch in range(n_batches):
@@ -42,36 +44,40 @@ for epoch in range(1, 1 + hyperparam['n_epochs']):
         y_batch = y_train[batch * hyperparam['batch_size'] : (1 + batch) * hyperparam['batch_size']]
 
         logits = model.forward(X_batch)
-        gradient = cross_entropy_gradient(logits, y_batch)
+        q = softmax(logits)
+        gradient = cross_entropy_gradient(q, y_batch)
         model.backward(gradient)
         model.step()
 
         if batch % 100 == 0:
-            logits = model.forward(X_train)
-            loss = cross_entropy_loss(logits, y_train).mean()
-            accuracy_train = accuracy(logits, y_train)
+            loss_train, accuracy_train = loss_and_accuracy(model, X_train, y_train)
+            loss_valid, accuracy_valid = loss_and_accuracy(model, X_valid, y_valid)
 
-            logits_valid = model.forward(X_valid)
-            loss_valid = cross_entropy_loss(logits_valid, y_valid).mean()
-            accuracy_valid = accuracy(logits_valid, y_valid)
+            print(f"Epoch {epoch:>2}; batch {1 + batch:>4}, train loss = {loss_train:.3f} ({accuracy_train*100:.1f}%), valid loss = {loss_valid:.3f} ({accuracy_valid*100:.1f}%)")
 
-            print(f"Epoch {epoch:>2}; batch {1 + batch:>4}, train loss = {loss:.3f} ({accuracy_train*100:.1f}%), valid loss = {loss_valid:.3f} ({accuracy_valid*100:.1f}%)")
-
+    loss_valid, accuracy_valid = loss_and_accuracy(model, X_valid, y_valid)
+    if loss_valid < best_loss:
+        best_model = copy.deepcopy(model)
+        best_loss = loss_valid
+    
     model.lr *= hyperparam['lr_decay']
 
+loss_train, accuracy_train = loss_and_accuracy(best_model, X_train, y_train)
+loss_valid, accuracy_valid = loss_and_accuracy(best_model, X_valid, y_valid)
+loss_test , accuracy_test  = loss_and_accuracy(best_model, X_test , y_test)
 
-logits_valid = model.forward(X_valid)
-loss_valid = cross_entropy_loss(logits_valid, y_valid).mean()
-accuracy_valid = accuracy(logits_valid, y_valid)
-
+print()
 print("Training done.")
+
+print()
+print(f"Accuracy on training set: {accuracy_train*100:.1f}%")
+print(f"Loss on training set: {loss_train:.4f}")
+
+print()
 print(f"Accuracy on validation set: {accuracy_valid*100:.1f}%")
 print(f"Loss on validation set: {loss_valid:.4f}")
-
-logits_test = model.forward(X_test)
-loss_test = cross_entropy_loss(logits_test, y_test).mean()
-accuracy_test = accuracy(logits_test, y_test)
 
 print()
 print(f"Accuracy on test set: {accuracy_test*100:.1f}%")
 print(f"Loss on test set: {loss_test:.4f}")
+print()