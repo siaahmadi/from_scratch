05e47c06fb98f59d9abf222ed1e7856fc219cfa2
Sia Ahmadi <siavash.a.ahmadi@gmail.com>
2026-02-25 22:21:29 -0800

    merge utils mlp and transformer

--- dl_from_scratch/utils/helpers.py
+++ dl_from_scratch/utils/helpers.py
@@ -1,9 +1,17 @@
 import numpy as np
 
-def softmax(x):
-    x -= np.max(x, axis=1, keepdims=True)
-    exp = np.exp(x)
-    return exp / exp.sum(axis=1, keepdims=True)
+def softmax(x, axis=None):
+    x -= np.max(x, axis=axis, keepdims=True)
+    exp_x = np.exp(x)
+    sum_exp_x = np.sum(exp_x, axis=axis, keepdims=True)
+    sum_exp_x[sum_exp_x==0] = 1 # avoid div-by-zero
+    return exp_x / sum_exp_x
+
+def masked_softmax(x, mask, axis=None):
+    if mask is not None:
+        mask = mask.astype(bool)
+        x[mask] = -np.inf
+    return softmax(x, axis=axis)
 
 def cross_entropy_loss(logits, p):
     logits -= np.max(logits, axis=1, keepdims=True)
@@ -26,3 +34,31 @@ def loss_and_accuracy(model, X, true_label):
     accuracy = np.mean(predicted == true)
     
     return loss, accuracy
+
+def xavier_uniform(shape, seed=None):
+    rng = np.random.default_rng(seed=seed)
+    d_in = shape[-2]
+    var_factor = 1/np.sqrt(d_in)
+    return rng.uniform(-1, 1, size=shape) * var_factor
+
+def xavier_normal(shape, seed=None):
+    rng = np.random.default_rng(seed=seed)
+    d_in = shape[-2]
+    var_factor = 1/np.sqrt(d_in)
+    return rng.standard_normal(shape) * var_factor
+
+
+if __name__ == "__main__":
+    seed = 0
+
+    rng = np.random.default_rng(seed=seed)
+
+    N, D = 8, 4
+
+    x = rng.standard_normal((N, D))
+
+    mask = np.ones(x.shape, dtype=np.bool)
+
+    y = masked_softmax(x, mask=mask, axis=1)
+
+    print(y)
